f1fc33266132de9bed3d3c5d76030e55734d278c
Lucas <bigluc23@gmail.com>
2017-03-14 15:12:23 -0400

    javascript problems

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -5,7 +5,8 @@
 $(function () {
   $('.fluidbox-trigger').fluidbox();
   
-  if ($(window).width() < 400){
+  if ($(window).width() < 400)
+  {
     $('#itunes-tab').text("iTunes");
     console.log("hi")
   }
